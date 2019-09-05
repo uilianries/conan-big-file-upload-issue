c09aeed8611ce74fff8e55cd7efdeff90c2c4f1b
Uilian Ries <uilianries@gmail.com>
2019-09-05 17:31:01 -0300

    no copy source
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -5,6 +5,7 @@ class FooConan(ConanFile):
     name = "foo"
     version = "0.1.0"
     exports_sources = "sample.txt"
+    no_copy_source = True
 
     def package(self):
-        self.copy("sample.txt", dst="res")
+        self.copy("sample.txt", dst="res", src=self.source_folder)
